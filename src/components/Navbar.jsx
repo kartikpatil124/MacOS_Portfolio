3536f4f42ea43c76bfb95c6ca771e187c29f0f34
Kartik Patil <kartik.patil12@outlook.com>
2026-06-03 17:55:16 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import dayjs from "dayjs";
+import { navLinks, navIcons } from "../constants"
+
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className="font-bold">Kartik's Portfolio</p>
+
+                <ul>
+                    {navLinks.map(({ id, name }) => (
+                        <li key={id}>
+                            <a href="#">{name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                        </li>
+                    ))}
+                </ul>
+
+                <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+            </div>
+
+
+        </nav>
+    )
+}
+
+export default Navbar
